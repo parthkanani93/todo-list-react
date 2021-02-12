abde5ce248de9e8ecdc406292450f3c2fba03b3d
jeel <jeelbhanvadiya781998@gmail.com>
2021-02-12 18:53:15 +0530

    complete todo in redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,19 @@
-import {createStore , applyMiddleware} from 'redux';
+import {createStore , applyMiddleware, compose } from 'redux';
 import logger from 'redux-logger';
 import rootreducer from './root-reducer';
 
 const middlewares = [logger];
-export const store = createStore(rootreducer ,applyMiddleware(...middlewares));
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose;
+
+    const enhancer = composeEnhancers(
+        applyMiddleware(...middlewares),
+        // other store enhancers if any
+      )
+
+export const store = createStore(rootreducer ,enhancer);
 // console.log(store.getState())
